0d0bbc53e9759b0af60f1e313db76f30c6aac8a5
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-02-25 17:58:01 +0200

    Modify server and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,67 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeJsPlugin = require('optimize-js-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OptimizeJsPlugin = require("optimize-js-plugin");
 
-const plugins = [new HtmlWebpackPlugin({
-    template: 'client/index.html',
-    filename: 'index.html',
-    inject: 'body'
-  })];
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "client/index.html",
+    filename: "index.html",
+    inject: "body"
+  })
+];
 
-module.exports = (env) => {
-    const environment = env || 'production';
-    if (env === 'production') {
-        plugins.push(
-            new OptimizeJsPlugin({
-                sourceMap: false
-            })
-        )
-    }
+module.exports = env => {
+  const environment = env || "production";
+  if (env === "production") {
+    plugins.push(
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    );
+  }
 
-    return {
-        mode: environment,
-        entry: './client/index.js',
-        output: {
-            path: path.resolve(__dirname, 'public'),
-            filename: 'app.bundle.js'
+  return {
+    mode: environment,
+    entry: "./client/index.js",
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: "app.bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            plugins: env !== "production" ? ["react-hot-loader/babel"] : []
+          }
         },
-        module: {
-            rules: [{
-                    test: /\.js$/,
-                    loader: "babel-loader",
-                    options: {
-                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-                    }
-                },
-                {
-                    test: /\.css$/,
-                    use: [{
-                            loader: 'style-loader'
-                        },
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                modules: true
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        
-        devServer: {
-            proxy: {
-                '/socket.io': {
-                    target: 'http://localhost:3000',
-                    ws: true
-                }
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              options: {
+                modules: true
+              }
             }
-          },
+          ]
+        }
+      ]
+    },
 
-          plugins: plugins
+    devServer: {
+      proxy: {
+        "/socket.io": {
+          target: "http://localhost:3000",
+          ws: true
+        }
+      }
+    },
 
-    }
-}
+    plugins: plugins
+  };
+};
